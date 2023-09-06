81a1657591f3451b448134eb7fa20c0264e3f0b6
Luan Alves <97049835+Luan029@users.noreply.github.com>
2023-09-06 08:42:38 -0300

    fazendo alguns ajustes na condição de login.

--- src/Components/login/Login.jsx
+++ src/Components/login/Login.jsx
@@ -10,46 +10,62 @@ const Login = () => {
     const [password, setPassword] = useState('');
     const [erro, setErro] = useState('')
     const navigate = useNavigate();
-    console.log(localStorage.getItem.value)
+    // Função que estava dando certo a requisição mas não a verificação do token.
+    // const handleSubmit = async (event) => {
+    //     try{
+    //         let url = 'http://127.0.0.1:8000/api-token-auth/'
+    //         // Simple POST request with a JSON body using fetch
+    //         const requestOptions = {
+    //             method: 'POST',
+    //             headers: { 'Content-Type': 'application/json' },
+    //             body: JSON.stringify({ username: username, password: password })
+    //         };
+    //         fetch(url, requestOptions)
+    //             .then(response => response.json())
+    //             .then(data => {
+    //                 localStorage.setItem('token', data.token)
+    //                 setUsername({token: data.token})
+    //                 setPassword({token: data.token})
+    //                 const isAuth= localStorage.getItem('token');
+    //                 if(isAuth){
+    //                     navigate("/Home");
+    //                 }else{
+    //                     setUsername('')
+    //                     setPassword('')
+    //                     setErro('O usuário ou a senha estão incorretos.')
+    //                 }
+    //         });
+    //         event.preventDefault()
+    //     }catch (error) {            
+    //         console.error(error);
+    //     }
+    // }
     const handleSubmit = async (event) => {
-        try{
-            let url = 'http://127.0.0.1:8000/api-token-auth/'
-            // Simple POST request with a JSON body using fetch
+        try {
+            let url = 'http://127.0.0.1:8000/api-token-auth/';
             const requestOptions = {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ username: username, password: password })
             };
-            fetch(url, requestOptions)
-                .then(response => response.json())
-                .then(data => {
-                    if (response.ok) {
-                        localStorage.setItem('token', data.token);
-                        navigate("/Home");
-                    } else {
-                        setUsername('');
-                        setPassword('');
-                        setErro('O usuário ou a senha estão incorretos.');
-                        localStorage.removeItem('token'); // Remova o token se a autenticação falhar.
-                    }
-                    // localStorage.setItem('token', data.token)
-                    // setUsername({token: data.token})
-                    // setPassword({token: data.token})
-                    // const isAuth= localStorage.getItem('token');
-                    // if(isAuth){
-                    //     navigate("/Home");
-                    // }else{
-                    //     setUsername('')
-                    //     setPassword('')
-                    //     setErro('O usuário ou a senha estão incorretos.')
-                    // }
-            });
-            event.preventDefault()
-        }catch (error) {            
+            const response = await fetch(url, requestOptions);
+            const data = await response.json();
+
+            if (response.ok) {
+                localStorage.setItem('token', data.token);
+                navigate("/Home");
+            } else {
+                setUsername('');
+                setPassword('');
+                setErro('O usuário ou a senha estão incorretos.');
+                localStorage.removeItem('token'); 
+            }
+
+            event.preventDefault();
+        } catch (error) {            
             console.error(error);
         }
     }
-
     return (
         <React.Fragment>
             <Header link={'/'}/>
